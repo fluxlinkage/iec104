c6fc31920ee5c698e008e930c829132eb8f07fa0
fluxlinkage <fluxlinkage@sina.com>
2025-11-07 14:41:21 +0800

    Add an extra callback

--- src/client.rs
+++ src/client.rs
@@ -112,6 +112,8 @@ impl AsyncWrite for Connection {
 #[async_trait]
 pub trait OnNewObjects {
 	async fn on_new_objects(&self, asdu: Asdu);
+	#[cfg(feature = "extra-callbacks")]
+	async fn on_close(&self);
 }
 
 pub struct Client {
--- src/client/connection_handler.rs
+++ src/client/connection_handler.rs
@@ -94,6 +94,8 @@ impl ConnectionHandler {
 						}
 					} else {
 						tracing::error!("Error receiving command. Aborting...");
+						#[cfg(feature = "extra-callbacks")]
+						self.callback.on_close().await;
 						whatever!("Error receiving command.");
 					}
 				}
@@ -141,6 +143,8 @@ impl ConnectionHandler {
 				ConnectionHandlerState::Reconnecting => {
 					if !self.config.auto_reconnect {
 						tracing::debug!("Reconnecting disabled");
+						#[cfg(feature = "extra-callbacks")]
+						self.callback.on_close().await;
 						whatever!("Reconnecting disabled.");
 					}
 					tracing::debug!("Reconnecting");
